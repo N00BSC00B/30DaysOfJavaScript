c5dec0257903bd2b2a1a680338874b5b1c1a6428
Sayan Barma <sayanbarma2004@gmail.com>
2024-03-03 23:51:53 +0530

    Day 1

---
+++ Day 1/8.js
@@ -0,0 +1,7 @@
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let firstName = "Sayan";
+let secondName = "Barma";
+let isMarried = false;
+let country = "India";
+let age = 19;
